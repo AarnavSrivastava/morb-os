81d3fbcb066e27ff4e4ec930be5a1226114b8de5
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-03-13 09:14:23 -0400

    wtf

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
-#![no_std]
-#![no_main]
-
-// rustup update on startup!
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
 
 static HELLO: &[u8] = b"Hello World!";
 
@@ -22,5 +16,11 @@ pub extern "C" fn _start() -> ! {
         }
     }
 
+    loop {}
+}
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
