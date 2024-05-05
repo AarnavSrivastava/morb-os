1e2099940350f8e3fd7ead7346a8b5ad577c39e9
Aarnav Srivastava <70161949+AarnavSrivastava@users.noreply.github.com>
2024-05-05 15:19:37 -0400

    msg

--- src/allocator/buddy.rs
+++ src/allocator/buddy.rs
@@ -8,9 +8,44 @@ struct ListNode {
     next: Option<&'static mut ListNode>,
 }
 
-const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+const BLOCK_SIZES: &[usize] = &[8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192];
 
 fn list_index(layout: &Layout) -> Option<usize> {
     let required_block_size = layout.size().max(layout.align());
     BLOCK_SIZES.iter().position(|&s| s >= required_block_size)
+}
+
+struct BuddyAllocator {
+    size: usize,
+    list_heads: [Option<&'static mut ListNode>; HEAP_SIZE],
+    fallback_allocator: linked_list_allocator::Heap,
+}
+
+impl BuddyAllocator {
+    /// Creates an empty FixedSizeBlockAllocator.
+    pub const fn new() -> Self {
+        const EMPTY: Option<&'static mut ListNode> = None;
+        BuddyAllocator {
+            size: HEAP_SIZE,
+            list_heads: [EMPTY; HEAP_SIZE],
+            fallback_allocator: linked_list_allocator::Heap::empty(),
+        }
+    }
+
+    /// Initialize the allocator with the given heap bounds.
+    ///
+    /// This function is unsafe because the caller must guarantee that the given
+    /// heap bounds are valid and that the heap is unused. This method must be
+    /// called only once.
+    pub unsafe fn init(&mut self, heap_start: usize, heap_size: usize) {
+        self.fallback_allocator.init(heap_start, heap_size);
+    }
+
+    /// Allocates using the fallback allocator.
+    fn fallback_alloc(&mut self, layout: Layout) -> *mut u8 {
+        match self.fallback_allocator.allocate_first_fit(layout) {
+            Ok(ptr) => ptr.as_ptr(),
+            Err(_) => ptr::null_mut(),
+        }
+    }
 }
